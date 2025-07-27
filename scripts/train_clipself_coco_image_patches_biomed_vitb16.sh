6cfc1617956b08596824710288b24fe52371c41e
mertsonmezer <mertsonmezer0106@gmail.com>
2025-07-27 06:58:09 +0300

    Make CustomTextCLIP ViT compatible

--- scripts/train_clipself_coco_image_patches_biomed_vitb16.sh
+++ scripts/train_clipself_coco_image_patches_biomed_vitb16.sh
@@ -1,4 +1,4 @@
-NPROC_PER_NODE=1
+NPROC_PER_NODE=4
 
 torchrun --nproc_per_node $NPROC_PER_NODE -m training.main \
   --name clipself_coco_6_save6_test1_biomed_vitb16_12layers \
@@ -9,14 +9,14 @@ torchrun --nproc_per_node $NPROC_PER_NODE -m training.main \
   --lr 1e-5 \
   --wd 0.1 \
   --alpha 0.7 \
-  --train-ratio 1.0 \
+  --train-ratio 0.05 \
   --downsample-factor 16 \
   --det-image-size 224 \
   --workers 4 \
   --warmup 1000 \
   --lock-image \
   --lock-image-unlocked-groups 12 \
-  --extract-type v1 \
+  --extract-type v2 \
   --dataset-type grid_distill \
   --test-type coco_panoptic \
   --train-data data/coco/annotations/instances_train2017.json \
--- scripts/train_clipself_coco_image_patches_eva_vitb16.sh
+++ scripts/train_clipself_coco_image_patches_eva_vitb16.sh
@@ -1,4 +1,4 @@
-NPROC_PER_NODE=1
+NPROC_PER_NODE=4
 
 torchrun --nproc_per_node $NPROC_PER_NODE -m training.main \
   --name clipself_coco_6_save6_test1_eva_vitb16_12layers \
